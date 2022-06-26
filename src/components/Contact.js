e059761766defcc4d7238c8605c91a2200180275
felixst10 <theofelix501@gmail.com>
2022-06-26 22:27:04 +0700

    init tugas 9

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import "./Contact.css";
 
-const Contact = () => {
+const Contact = ({profile}) => {
     return (
-        <></>
+        <div class="contact">
+      <img class="photo" src={profile.photo}></img>
+      <div class="profile">
+        <p>{profile.name}</p>
+        <p>{profile.phone}</p>
+        <p>{profile.email}</p>
+      </div>
+    </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
+
+
 const Header = () => {
     return (
-        <></>
+        <>
+      <h1>Call a Friend</h1>
+      <p>your friendly contact app</p>
+      <p class="divider">
+        ***************************************************************
+      </p>
+    </>
     )
 }
 
